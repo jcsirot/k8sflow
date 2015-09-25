cb286ca4717409a12d99a4879b1e75daca8e3d99
Antoine Legrand <antoine.legrand@arkena.com>
2015-09-25 15:35:09 +0200

    Add defaults

--- lib/k8sflow.rb
+++ lib/k8sflow.rb
@@ -4,8 +4,8 @@ require 'k8sflow/cli'
 require 'k8sflow/command'
 
 Clitopic.version = K8sflow::VERSION
-Clitopic.parser = Clitopic::PARSERS[:optparse]
+Clitopic.parser = Clitopic::Parser::OptParser
 Clitopic.commands_dir = "#{__FILE__}/k8sflow/command"
+Clitopic.default_files = [File.join(Dir.getwd, ".k8sflow.yml"), File.join(Dir.home, ".k8sflow.yml")]
 
-K8sflow::Run::Index.defaults = {docker_api: 'tcp://localhost:4243', envs: ['FORCE_HTTPS=true', 'RAILS_ENV=production'], repo: 'arkena/osm-backend'}
-K8sflow::Run::Index.shortcuts = {'c' => 'rails console', 's' => 'rails server'}
+K8sflow::Run::Index.shortcuts = {'c' => 'rails console', 's' => 'rails server', 'console' => "rails console", "server" => "rails server"}
